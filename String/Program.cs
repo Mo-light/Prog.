0845a87754cad9bc8effb671032e245e0078e62a
Mo-light <147440730+Mo-light@users.noreply.github.com>
2023-11-24 15:31:45 +0000

    Debuged

--- String/Program.cs
+++ String/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Getting rid of it");
+﻿using System.ComponentModel;
+using System.Runtime.Intrinsics.Arm;
+
+Console.WriteLine(Contains("Grünes Gras", "Gras"));
+
+
+static bool Contains(string ersteEingabe, string zweiteEingabe)
+{
+    for (int i = 0; i < ersteEingabe.Length - zweiteEingabe.Length +1; i++)
+    {
+        if (ersteEingabe[i] == zweiteEingabe[0])
+        {
+            int j = 0;
+            while (ersteEingabe[i + j] == zweiteEingabe[j])
+            {
+                j++;
+            }
+            if (j == zweiteEingabe.Length)
+            {
+                return true;
+            }
+        
+        }
+    }
+    return false;
+}
