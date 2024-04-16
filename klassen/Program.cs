a5733bb28c6e9acf46e5d7a8e1a6818a220eb5f9
Mo-light <147440730+Mo-light@users.noreply.github.com>
2024-04-16 18:06:35 +0000

    Initial Commit

---
+++ klassen/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+class Konto
+{
+    private int nr;
+    private double saldo;
+    private double dispo;
+
+
+    public Konto(int _nr)
+    {
+        nr = _nr;
+        dispo = 1000;
+        saldo = 0;
+    }
+
+    public void Einzahlen(double betrag)
+    {
+        if ( betrag >= 0)
+        {
+            saldo +=betrag;
+        }
+        
+    }
+
+    public void Auszahlen(double betrag)
+    {
+        if (betrag >= 0)
+        {
+            if ( saldo + dispo >= betrag)
+            {
+                saldo -= betrag;
+            }
+        }
+    }
+
+    public double Saldo()
+    {
+        return saldo;
+    }
+
+    public void SetDipo(double d)
+    {
+        if (d >= 0)
+        {
+            dispo = d;
+        }
+    
+    }
+
+    //public void Uberweisung(double betrag, Konto k)
+    //{
+    //    Auszahlen(betrag);
+    //    k.Einzahlen(betrag);
+    //}
+  
+}
---
+++ klassen/runKlassen/Program.cs
@@ -0,0 +1,5 @@
+﻿Konto k1 = new Konto(123);
+k1.Einzahlen(6);
+Console.WriteLine(k1.Saldo());
+k1.Auszahlen(0);
+Console.WriteLine(k1.Saldo());
